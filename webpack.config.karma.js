9c0585d1cc2306b40a586f43ff4fe37a2ef2c7ec
uryyyyyyy <koki305@gmail.com>
2016-08-29 23:59:46 +0900

    karma test

--- webpack.config.karma.js
+++ webpack.config.karma.js
@@ -1,11 +1,4 @@
 module.exports = {
-  entry: {
-    app: __dirname + '/src/Index.tsx'
-  },
-  output: {
-    path: __dirname + '/dist',
-    filename: "[name].bundle.js"
-  },
   resolve: {
     extensions: ['', '.ts', '.js', ".tsx"]
   },
