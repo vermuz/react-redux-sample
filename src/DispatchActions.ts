6c47b7a764e4eca2280ca508eadb8a8b420ccb2b
ko.shibata <ko.shibata@opt.ne.jp>
2016-09-08 16:10:08 +0900

    async update

--- src/DispatchActions.ts
+++ src/DispatchActions.ts
@@ -1,6 +1,5 @@
 import * as request from 'superagent';
 import {ActionTypes, JsonObject} from "./Entities";
-import {Response} from "superagent";
 
 export class DispatchActions {
     private dispatch: (action: any) => any;
@@ -22,7 +21,7 @@ export class DispatchActions {
             this.dispatch({ type: ActionTypes.FETCH_FAIL})
         };
 
-        const successCB = (res:Response) => {
+        const successCB = (res:request.Response) => {
             const json:JsonObject = res.body;
             const action = { type: ActionTypes.FETCH_SUCCESS, amount: json.amount};
             this.dispatch(action)
